74149238bdc91a73a7bdd0f69fd160ee90c2f367
Logan Kelly <logan.r.kelly@gmail.com>
2017-07-22 17:10:50 -0500

    npm packages added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import './styles/index.css';
+
+const app = document.getElementById('app');
+
+
+ReactDOM.render(<App></App>, app);
