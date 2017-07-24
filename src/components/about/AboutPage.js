5d5957973e34f11658fe8f0cc23ae35785aaba1d
Logan Kelly <logan.r.kelly@gmail.com>
2017-07-24 02:39:38 -0500

    added text and github link to about page

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -7,7 +7,9 @@ class AboutPage extends React.Component {
         return (
             <div>
                 <h1>About</h1>
-                <p>Check the package.json for included libraries</p>
+                <a href="https://github.com/gnargnor/react-04-react-with-routing-starter">View Source Code - Github Repository</a>
+                <p>The source code for this application will help create a starter skeleton for a React and Redux application with preconfigured webpack instructions and routing examples.</p>
+                <p>Check the package.json for all included libraries</p>
             </div>
         );
     }
